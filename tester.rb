60db7f5f82c2f3e726f5c2ae21cc25bb98b094b7
Adam Beguelin <adam@beguelin.com>
2016-03-27 09:52:36 -0400

    read the header

--- tester.rb
+++ tester.rb
@@ -1,14 +1,16 @@
+require 'pp'
+
 # FF D8 start of image
 # FF D9 end of image
 # FFD0 - FFD9 and FF01 are not followed by length
+START = 0xffd8
+JPGEND = 0xffd9
 
-open('bad.jpg', 'rb') do |io| 
-  io.each_byte do |b| 
-#    puts b.to_s(16)   
-    puts "valid header" if b == 0xd8
-    case b
-    when b == 0xd8 
-      puts "valid header"
+open('good.jpg', 'rb') do |io| 
+  while b = io.read(2)
+    d = b.unpack('n').first
+    puts "valid header" if d == START
+    puts "valid end" if d == JPGEND
   end
 end
 
